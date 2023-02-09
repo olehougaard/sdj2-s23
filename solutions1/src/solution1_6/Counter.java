f091cde38737c663266cc2a354461e03d48a4882
Ole Ildsgaard Hougaard <oih@via.dk>
2023-02-09 08:50:02 +0100

    Initial commit

---
+++ solutions1/src/solution1_6/Counter.java
@@ -0,0 +1,17 @@
+package solution1_6;
+
+public class Counter {
+    private int count;
+
+    public Counter() {
+        this.count = 0;
+    }
+
+    public void incrementCount() {
+        this.count++;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+}
---
+++ solutions1/src/solution1_6/CounterIncrementer.java
@@ -0,0 +1,15 @@
+package solution1_6;
+
+public class CounterIncrementer implements Runnable {
+    private Counter counter;
+
+    public CounterIncrementer(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for(int i = 0; i < 1_000_000; i++) {
+            counter.incrementCount();
+        }
+    }
+}
